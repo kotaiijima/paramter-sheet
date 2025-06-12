9d813093cee6983f9c0f613a8f21d2d294c9d0c5
kotaiijima <kota.iijima@icloud.com>
2025-06-12 13:36:39 +0900

    first commit

--- home.py
+++ home.py
@@ -1,186 +1,105 @@
 from flask import Flask, render_template, request
-import psycopg2
-import psycopg2.extras
-from decimal import *
+import openpyxl
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill, Border, Side
+import datetime
+import logging
+import os
+from tkinter import filedialog
+LOGFILE_NAME = "DEBUG.log"
 
 app = Flask(__name__)
 
-@app.route('/', methods=["POST","GET"])
+app.logger.setLevel(logging.WARN)
+log_handler = logging.FileHandler(LOGFILE_NAME)
+log_handler.setLevel(logging.WARN)
+app.logger.addHandler(log_handler)
+
+# 枠線なしのスタイル設定
+no_border = Border(left=Side(border_style=None),
+                   right=Side(border_style=None),
+                   top=Side(border_style=None),
+                   bottom=Side(border_style=None))
+
+#印刷範囲
+print_area = 'A1:AU71'
+
+@app.route("/", methods=["GET", "POST"])
 def home():
+    message = ""
 
-        # データベースに接続
-    connection = psycopg2.connect(host='localhost',
-                              user='postgres',
-                              password='Taka1579',
-                              database='sampledb')
-    if request.method == 'POST':
-        command = request.form['command']
-        match command:
-            case '追加':
-                tensu_1 = request.form['tensu_1']
-                tensu_2 = request.form['tensu_2']
-                tensu_3 = request.form['tensu_3']
-                tensu_4 = request.form['tensu_4']
-                insert(connection, tensu_1, tensu_2, tensu_3, tensu_4)
-            case '更新':
-                id = request.form['id']
-                tensu_1 = request.form['tensu_1']
-                tensu_2 = request.form['tensu_2']
-                tensu_3 = request.form['tensu_3']
-                tensu_4 = request.form['tensu_4']
-                update(connection, id, tensu_1, tensu_2, tensu_3, tensu_4)
-            case '削除':
-                id = request.form['id']
-                delete(connection, id)
-        
-    tensu_list, shokin = set_result(connection)
- 
-    return render_template(
-        'home.html',
-        title='年末麻雀大会',
-        tensu_list = tensu_list,
-        shokin = shokin
-        )
+    # ファイル名のフォーマット修正
+    excel_file_name = 'パラメーターシート-%s.xlsx' % "パーキテック"
 
-def insert(connection, tensu_1, tensu_2, tensu_3, tensu_4):
-        with connection:
-            with connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
-        # レコードを追加
-                sql = "INSERT INTO majan (tensu_1, tensu_2, tensu_3 ,tensu_4) VALUES(%s, %s, %s, %s)"
-                print(sql)
-                cursor.execute(sql, (tensu_1, tensu_2, tensu_3, tensu_4))
-                connection.commit()
+    # Excelファイルの作成と保存
+    create_file = openpyxl.Workbook()
+    create_file.save(excel_file_name)
+    excel_file = openpyxl.load_workbook(excel_file_name)
 
-def update(connection, id, tensu_1, tensu_2, tensu_3, tensu_4):
-        with connection:
-            with connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
-        # レコードを追加
-                sql = "UPDATE majan set tensu_1= %s, tensu_2= %s, tensu_3= %s, tensu_4= %s WHERE id = %s"
-                print(sql)
-                cursor.execute(sql, (tensu_1, tensu_2, tensu_3, tensu_4, id))
-                connection.commit()
+    # シート名
+    sheet_list = ['表紙', '本体設定', '電源設定', 'バックアップ設定']
 
-def delete(connection, id):
-            with connection:
-                with connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
-            # レコードを追加
-                    sql = "DELETE majan WHERE id = %s"
-                    print(sql)
-                    cursor.execute(sql, (id))
-                    connection.commit()
+    for sheet_name in sheet_list:
+        excel_file.create_sheet(title=sheet_name)	
 
-@app.route('/insert', methods=["POST"])
-def insert_bk(): 
-    # データベースに接続
-    connection = psycopg2.connect(host='localhost',
-                              user='postgres',
-                              password='Taka1579',
-                              database='sampledb')
+    excel_file.remove(excel_file['Sheet'])	
+    excel_file.save(excel_file_name)
+
+    # 書き込むExcelシートを選択
+    sheet = excel_file[sheet_list[1]]
+    #印刷範囲を指定
+    sheet.print_area = print_area
+    sheet.print_options.page_breaks = True
+    sheet.page_setup.scale = 60
+    # 書き込み始める行と列を定義
+    start_row = 1
+    start_col = 1
+    end_row = 47
+    end_col = 71
+    #行と列の幅を調整
+    row_height = 17.5
+    col_width = 2.5
+    #セルの枠線を非表示、１ページの枠にする
+    for y in range (start_col, end_col + 1):
+        for x in range (start_row, end_row + 1):
+            cell = sheet.cell(row=y, column=x)
+            cell.border = no_border
+            if y == 1:
+                sheet.column_dimensions[cell.column_letter].width = col_width
+            sheet.row_dimensions[y].height = row_height                
+
+    excel_file.save(excel_file_name)
+
+    #セルの塗りつぶし設定
+    pf_blue1 = PatternFill(start_color="8EA9D8", end_color="8EA9D8", fill_type="solid")
+    #塗りつぶし枠の範囲を設定
+    frame1 = 38
     
-    tensu_1 = request.form['tensu_1']
-    tensu_2 = request.form['tensu_2']
-    tensu_3 = request.form['tensu_3']
-    tensu_4 = request.form['tensu_4']
-    print(tensu_1)
+    if request.method == "POST":
+        print(request.form)
+        if "submit_button" in request.form and request.form["submit_button"] == "clicked":
+            excel_file.save(excel_file_name)
+        # 書き込み
+        #for y in range (1, frame1 + 1):
+        #    for x, cell_value in enumerate(row):
+        #        cell = sheet.cell(row=start_row + y, column=start_col + x, value=cell_value)	
+        #        cell.fill = pf_blue1
 
-    with connection:
-        with connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
-        # レコードを追加
-            sql = "INSERT INTO majan (tensu_1, tensu_2, tensu_3 ,tensu_4) VALUES(%s, %s, %s, %s)"
-            print(sql)
-            cursor.execute(sql, (tensu_1, tensu_2, tensu_3, tensu_4))
-            connection.commit()
+    # ファイル保存ダイアログを表示（ファイル名も指定可能）
 
-    tensu_list, shokin = set_result(connection)
+            filepath = filedialog.asksaveasfilename(
+                defaultextension=".xlsx",
+                filetypes=[("Text files", "*.xlsx"), ("All files", "*.*")],
+                title="保存先を指定"
+            )
 
-    return render_template(
-        'home.html',
-        title='年末麻雀大会',
-        tensu_list = tensu_list,
-        shokin = shokin
-        )
+            if filepath:
+                excel_file.save(filepath)
+                message = print(f"ファイルを保存しました: {filepath}")
 
-@app.route('/update', methods=["POST"])
-def update(): 
-    # データベースに接続
-    connection = psycopg2.connect(host='localhost',
-                              user='postgres',
-                              password='Taka1579',
-                              database='sampledb')
+    os.remove(excel_file_name)
     
-    tensu_1 = request.form['tensu_1']
-    tensu_2 = request.form['tensu_2']
-    tensu_3 = request.form['tensu_3']
-    tensu_4 = request.form['tensu_4']
-    id = request.form['id']
+    return render_template("home.html", message=message)
 
-
-    with connection:
-        with connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
-        # レコードを更新
-            sql = "UPDATE majan set tensu_1= %s, tensu_2= %s, tensu_3= %s, tensu_4= %s WHERE id = %s"
-            print(sql)
-            cursor.execute(sql, (tensu_1, tensu_2, tensu_3, tensu_4, id))
-            connection.commit()
-
-    tensu_list, shokin = set_result(connection)
-
-    return render_template(
-        'home.html',
-        title='年末麻雀大会',
-        tensu_list = tensu_list,
-        shokin = shokin
-        )
-
-@app.route('/tuika')
-def tuika(): 
-
-    return render_template(
-        'tuika.html',
-        title='年末麻雀大会',
-        )
-
-
-def set_result(connection):
-
-    with connection:
-        with connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
-        # レコードを取得
-            sql = "SELECT * FROM majan"
-            cursor.execute(sql)
-            tensu_list = cursor.fetchall()
-        # レコードを取得
-            sql = "SELECT COUNT(*) FROM majan"
-            cursor.execute(sql)
-            count = cursor.fetchone()
-            sum_tensu = [0, 0, 0, 0]
-    for i in range(0, count[0]):
-        list = tensu_list[i][:4]
-        sort_list = sorted(list, reverse=True)
-        for j in range(0, 4):
-            idx = sort_list.index(list[j])
-            match idx:
-                case 0:
-                    list[j] += 40000
-                    sum_tensu[j] += list[j]
-                case 1:
-                    list[j] += 10000
-                    sum_tensu[j] += list[j]
-                case 2:
-                    list[j] -= 10000
-                    sum_tensu[j] += list[j]
-                case 3:
-                    list[j] -= 20000
-                    sum_tensu[j] += list[j]
-    shokin = [0, 0, 0, 0]
-    if count[0] != 0:
-        for i in range(4):
-            w_shokin = ((sum_tensu[i] / count[0] - 30000) / 10 + 5000) / 1000
-            shokin[i] = int(Decimal(str(w_shokin)).quantize(Decimal('0.1'), rounding=ROUND_HALF_DOWN) * 1000)
-        print(shokin)    
-    return tensu_list, shokin
-        
 if __name__ == '__main__':
     app.run(debug=True)
-
-    
